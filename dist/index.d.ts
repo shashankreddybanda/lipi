b977b7269fb9970a65d42d3ca28c501b67a87823
shashankreddybanda <shashank24140544@gmail.com>
2024-02-09 00:01:31 -0700

    added dist

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function isMyName(input: String): boolean;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isMyName = void 0;
+function isMyName(input) {
+    return input === "ShashankReddyBanda";
+}
+exports.isMyName = isMyName;
+//# sourceMappingURL=index.js.map
